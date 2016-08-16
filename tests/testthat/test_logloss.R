fc639df19cf06e7cd1e8314a7a9d9f7cae04d4ca
Tyler Hunt <tyler.hunt@utah.edu>
2016-08-16 10:08:39 -0600

    test for data.frame conversion

--- tests/testthat/test_logloss.R
+++ tests/testthat/test_logloss.R
@@ -20,6 +20,23 @@ test_that("mlogLoss character/factor actual", {
 })
 
 
+test_that("mlogLoss data.frame predicted", {
+
+  Levs = 8
+  Size = 100
+  y = sample(1:Levs, Size, replace = TRUE)
+
+  xm = matrix(runif(Levs*Size), ncol = Levs)
+  xm = xm/rowSums(xm)
+
+  m1 <- mlogLoss(y, xm)
+  m2 <- mlogLoss(as.character(y), data.frame(xm))
+
+  expect_true(m1 == m2)
+
+})
+
+
 test_that("logLoss estimates with 0s and 1s as values", {
 
   data(testDF)
