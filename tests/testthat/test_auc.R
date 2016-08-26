5c2d81c127fc0481d50a4bb1c56f4f9b485fb499
Tyler Hunt <tyler.hunt@utah.edu>
2016-08-26 09:06:04 -0600

    expanded tests

--- tests/testthat/test_auc.R
+++ tests/testthat/test_auc.R
@@ -15,3 +15,31 @@ test_that("auc binary error", {
 
 })
 
+
+test_that("mauc", {
+
+  Levs = 8
+  Size = 100
+  y = sample(1:Levs, Size, replace = TRUE)
+
+  xm = matrix(runif(Levs*Size), ncol = Levs)
+  xm = xm/rowSums(xm)
+  # no warnings
+  expect_silent(res1 <- mauc(y, xm))
+  # estimated
+  expect_true(!is.nan(res1$mauc))
+
+  expect_silent(res2 <- mauc(y, as.data.frame(xm)))
+  expect_true(res1$mauc == res2$mauc)
+  expect_true(all(res1$auc == res2$auc))
+
+  expect_silent(res3 <- mauc(as.character(y), as.data.frame(xm)))
+  expect_true(res1$mauc == res3$mauc)
+  expect_true(all(res1$auc == res3$auc))
+
+  expect_silent(res4 <- mauc(as.factor(y), as.data.frame(xm)))
+  expect_true(res1$mauc == res4$mauc)
+  expect_true(all(res1$auc == res4$auc))
+
+})
+
