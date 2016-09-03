23413485c3cda0abad2286a1dd18213873b5f274
Tyler Hunt <tyler.hunt@utah.edu>
2016-09-03 14:32:39 -0600

    added in hand calcs

--- tests/testthat/test_logloss.R
+++ tests/testthat/test_logloss.R
@@ -46,6 +46,33 @@ test_that("mlogLoss different classes", {
   expect_true(res1 == res4)
   expect_true(all(res1 == res4))
 
+
+  ## test taken from caret written by Max Kuhn
+  eps <- 1e-15
+
+  classes <- LETTERS[1:3]
+
+  test_dat1 <- data.frame(obs  = c("A", "A", "A", "B", "B", "C"),
+                          pred = c("A", "A", "A", "B", "B", "C"),
+                          A = c(1, .80, .51, .1, .2, .3),
+                          B = c(0, .05, .29, .8, .6, .3),
+                          C = c(0, .15, .20, .1, .2, .4))
+
+  expected1 <- log(1-eps) + log(.8) + log(.51) + log(.8) + log(.6) + log(.4)
+  expected1 <- -expected1/nrow(test_dat1)
+  result1 <- mlogLoss(test_dat1$obs, test_dat1[,3:5])
+
+  # test_dat2 <- test_dat1
+  # test_dat2$A[1] <- NA
+  #
+  # expected2 <-  log(.8) + log(.51) + log(.8) + log(.6) + log(.4)
+  # expected2 <- c(logLoss = -expected2/sum(complete.cases(test_dat2)))
+  # result2 <- mlogLoss(test_dat2$obs, test_dat2[,3:5])
+
+  expect_equal(result1, expected1)
+  # expect_equal(result2, expected2)
+  # expect_equal(result3, expected3)
+
 })
 
 
