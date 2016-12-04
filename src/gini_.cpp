583e6bfdedac3ef45ad77cb31e255895e6c85ae6
Tyler Hunt <tyler.hunt@utah.edu>
2016-12-03 22:09:01 -0700

    initial rewrite of gini. closes #6

---
+++ src/gini_.cpp
@@ -0,0 +1,28 @@
+#include <Rcpp.h>
+#include <omp.h>
+using namespace Rcpp;
+
+// Assumes that actual is sorted by predicted values
+// [[Rcpp::export]]
+double gini_(NumericVector actual) {
+
+  double n = actual.size();
+
+  double pop_delta = 1/n;
+  double total_loss = sum(actual);
+  NumericVector accum_loss = actual/total_loss;
+
+  Rcpp::NumericVector giniVector = Rcpp::no_init_vector(n);
+
+  #pragma omp parallel for
+  for(int i = 0; i < n; ++i) {
+    if(i == 0){
+      giniVector[i] = (accum_loss[i] - pop_delta);
+    } else {
+      giniVector[i] = giniVector[i-1] + (accum_loss[i] - pop_delta);
+    }
+  }
+  double gini = sum(giniVector)/n;
+  return gini;
+
+}
